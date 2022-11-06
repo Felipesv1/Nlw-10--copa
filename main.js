66bf97c4eecc331469708439924cd02de0db661b
Felipe Pereira <fepereirasilva4@gmail.com>
2022-11-06 14:25:43 -0300

    Mais jogos e horarios add

--- main.js
+++ main.js
@@ -19,12 +19,28 @@ createCard = (date,day,games) => {
         </div>`
 }
 
-document.querySelector("#cards").innerHTML = 
-      createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia")) +
-    createCard(
-        "28/11",
-        "segunda",
-        createGame("switzerland", "13:00", "brazil") +
-          createGame("portugal", "16:00", "uruguai")
-      ) +
-      createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "23/11",
+    "quarta",
+    createGame("germany", "10:00", "japan") +
+      createGame("spain", "13:00", "costa-rica")
+  ) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("switzerland", "13:00", "brazil") +
+      createGame("portugal", "16:00", "uruguai")
+  ) +
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("south-korea", "16:00", "portugal") +
+      createGame("brazil", "16:00", "cameroon")
+  )
